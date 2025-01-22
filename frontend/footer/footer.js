f0053033c4b8ed0faffa02218138ef00d20f0bac
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-01-22 10:42:43 +0200

    integration of footer and nav-bar as elements in every html page

--- frontend/footer/footer.js
+++ frontend/footer/footer.js
@@ -1,6 +1,6 @@
 class FooterBar extends HTMLElement {
     connectedCallback() {
-        fetch('footer.html')
+        fetch('footer/footer.html')  // Path to footer.html
             .then(response => response.text())
             .then(data => this.innerHTML = data)
             .catch(error => console.error('Error loading footer:', error));
