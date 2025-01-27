2f1d42106a3622b65add8db8882fd0d47f01b06e
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-01-27 11:42:43 +0200

    minor changes - card stack (not done, will replace)

--- frontend/JavaScriptFiles/card_stack.js
+++ frontend/JavaScriptFiles/card_stack.js
@@ -1,18 +1,37 @@
-let stack = document.querySelector(".stack");
+//card-stack-from-dashboard
+$(document).ready(function() {
 
-[...stack.children].reverse().forEach(i => stack.append(i));
+    var content = $('.content');
+    var currentItem = content.filter('.active');
+    var steps = $('.card').filter('.steps');
+    var inactive1 = $('.inactive-1');
+    var inactive2 = $('.inactive-2');
 
-stack.addEventListener("click", swap);
+    $('.button').click(function() {
+        var nextItem = currentItem.next();
+        var lastItem = content.last();
+        var contentFirst = content.first();
+        
+        currentItem.removeClass('active');
 
-function swap(e) {
-    
-    let card = document.querySelector(".card:last-child");
-    if (e.target !== card) return;
-    card.style.animation = "swap 700ms forwards";
+        if (currentItem.is(lastItem)) {
+            currentItem = contentFirst.addClass('active');
+            currentItem.css({'right': '10%', 'opacity': '1'});
+            $('.step').animate({width: '33%'});
+            inactive1.animate({height: '8px', marginLeft:'20px', marginRight:'20px'}, 100);
+            inactive2.animate({height: '8px', marginLeft:'10px', marginRight:'10px'}, 100);
+            
+        } else if (currentItem.is(contentFirst)) {
+            currentItem.animate({opacity: 0}, 1000);
+            currentItem = nextItem.addClass('active');
+            $('.step').animate({width: '66%'});
+            inactive2.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
+            
+        } else {
+            currentItem = nextItem.addClass('active');
+            $('.step').animate({width: '100%'});
+            inactive1.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
+        } 
+    });
 
-
-    setTimeout(() => {
-        card.style.animation = "";
-        stack.prepend(card);
-    },700);
-}
+});
