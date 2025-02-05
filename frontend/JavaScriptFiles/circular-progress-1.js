6e337fa97b5e5ea63119f30d3c3e3526d9f75e0c
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-02-05 13:31:10 +0200

    index circular progress bars

---
+++ frontend/JavaScriptFiles/circular-progress-1.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.circular-progress, .circular-progress-2, .circular-progress-3').forEach((circularProgress, index) => {
+    let progressValue = circularProgress.querySelector('.progress-value, .progress-value-2, .progress-value-3');
+    let progressStartValue = 0;
+    let progressEndValue = [77, 62, 80][index];
+    let speed = 15;
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+        progressValue.textContent = `${progressStartValue}%`;
+
+        circularProgress.style.background = `conic-gradient(#4070f4 ${progressStartValue * 3.6}deg, #ededed 0deg)`;
+
+        if (progressStartValue == progressEndValue) {
+            clearInterval(progress);
+        }
+    }, speed)
+});
