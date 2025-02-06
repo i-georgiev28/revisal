f776546f0aac770d3742902e1c07b7886e6aca42
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-02-06 16:32:29 +0200

    reverse

--- frontend/nav-bar-top/nav-top.js
+++
@@ -1,9 +0,0 @@
-class NavBar extends HTMLElement {
-    connectedCallback() {
-        fetch('nav-bar-top/nav-bar-top.html')
-            .then(response => response.text())
-            .then(data => this.innerHTML = data)
-            .catch(error => console.error('Error loading nav:', error));
-    }
-}
-customElements.define('nav-bar-top', NavBar);
--- frontend/nav-bar/nav.js
+++ frontend/nav-bar/nav.js
@@ -1,6 +1,6 @@
 class NavBar extends HTMLElement {
     connectedCallback() {
-        fetch('nav-bar/nav-bar-aside.html')
+        fetch('nav-bar/nav.html')
             .then(response => response.text())
             .then(data => this.innerHTML = data)
             .catch(error => console.error('Error loading nav:', error));
