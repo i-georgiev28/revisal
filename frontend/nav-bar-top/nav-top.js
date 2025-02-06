9a39fd3c21e120b01e7edfcabb2e867a783b7442
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-02-06 13:41:30 +0200

    integration - awaiting fixes

--- frontend/nav-bar-top/nav-top.js
+++ frontend/nav-bar-top/nav-top.js
@@ -1,6 +1,6 @@
 class NavBar extends HTMLElement {
     connectedCallback() {
-        fetch('nav-bar-top/nav-')
+        fetch('nav-bar-top/nav-bar-top.html')
             .then(response => response.text())
             .then(data => this.innerHTML = data)
             .catch(error => console.error('Error loading nav:', error));
